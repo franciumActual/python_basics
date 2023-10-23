e8b38ecb54575242ae73c585dc96068c6e534f4e
franciumActual <139164768+franciumActual@users.noreply.github.com>
2023-10-23 17:45:00 -0500

    cs - working with textual data

--- strings.py
+++ strings.py
@@ -1,3 +1,48 @@
-practice = "this will be about string manipulation"
+message = "hello world"
+'''
+print(message)  
 
-print(practice)
+print(len(message))
+
+#called slicing
+print("message[6] is:",message[6])
+
+print("message[3:9] is:",message[3:9])
+
+#a method is a function that belongs to a specific class
+print(message.upper())
+print(message.lower())
+
+
+#these are case sensitive 
+print(message.find('world'))
+print(message.count('world'))
+
+
+print(message.replace('world', 'universe'))
+print(message.replace('world', 'universe', 0)) #does not print because 3rd param is set to 0
+print(message.replace('world', 'universe', 1))
+
+
+
+
+greeting = 'hello'
+name = 'sandi'
+
+greeting_message = greeting + ' ' + name
+
+greeting_message_ref = f'{greeting} {name}. Welcome to our Company!'
+
+
+
+print(greeting_message)
+print(greeting_message_ref)
+
+print(dir())
+print(__file__)
+
+
+
+print(dir(message))
+
+'''
